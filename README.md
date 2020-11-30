843da0be05cf1b2208fe1d5323a335a5e52f32c5
Erik Bigler <erikbigler@gmail.com>
2020-11-30 01:29:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# firebot-custom-scripts-types
+ TypeScript types for Firebot's Custom Scripts
---
+++ index.d.ts
@@ -0,0 +1,112 @@
+export interface FirebotCustomScriptManifest {
+  name: string;
+  description: string;
+  version: string;
+  author: string;
+  website?: string;
+  startupOnly?: boolean;
+  firebotVersion?: "5";
+}
+
+type BaseParameter = {
+  description: string;
+  secondaryDescription: string;
+  showBottomHr: string;
+};
+
+type StringParameter = BaseParameter & {
+  type: "string";
+  default: string;
+};
+
+type PasswordParameter = BaseParameter & {
+  type: "password";
+  default: string;
+};
+
+type BooleanParameter = BaseParameter & {
+  type: "boolean";
+  default: boolean;
+};
+
+type NumberParameter = BaseParameter & {
+  type: "number";
+  default: number;
+};
+
+type EnumParameter = BaseParameter & {
+  type: "enum";
+  options: Array<string | number>;
+  default: string | number;
+};
+
+export type Parameter =
+  | StringParameter
+  | PasswordParameter
+  | BooleanParameter
+  | NumberParameter
+  | EnumParameter;
+export type DefaultParametersConfig = Record<string, Parameter>;
+
+type FirebotAccount = {
+  username: string;
+  displayName: string;
+  userId: string;
+  avatar: string;
+  loggedIn: boolean;
+  auth: {
+    access_token: string;
+    expires_at: string;
+    refresh_token: string;
+  };
+};
+type FirebotTriggerType =
+  | "command"
+  | "custom_script"
+  | "startup_script"
+  | "api"
+  | "event"
+  | "hotkey"
+  | "timer"
+  | "counter"
+  | "preset"
+  | "manual";
+
+export type RunRequest<P extends Record<string, any>> = {
+  parameters: P;
+  modules: any;
+  firebot: {
+    accounts: {
+      streamer: FirebotAccount;
+      bot: FirebotAccount;
+    };
+    settings: {
+      webServerPort: number;
+    };
+    version: string;
+  };
+  trigger: {
+    type: FirebotTriggerType;
+    metadata: {
+      username: string;
+      hotkey?: any;
+      command?: any;
+      userCommand?: any;
+      chatMessage?: any;
+      event?: { id: string; name: string };
+      eventSource?: { id: string; name: string };
+      eventData?: Record<string, unknown>;
+      [x: string]: unknown;
+    };
+  };
+};
+
+export interface FirebotCustomScript<P extends Record<string, any> = {}> {
+  getScriptManifest():
+    | FirebotCustomScriptManifest
+    | PromiseLike<FirebotCustomScriptManifest>;
+  getDefaultParameters(): DefaultParametersConfig;
+  run(runRequest: RunRequest<P>): void;
+}
+
+export as namespace FirebotCustomScripts;
