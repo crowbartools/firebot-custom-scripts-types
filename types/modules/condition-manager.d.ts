636c0bf2e013c07230da3c680c127dd1976ac9f8
ebiggz <erikbigler@gmail.com>
2022-05-14 03:03:50 -0500

    updates to condition manager to make it more typesafe

--- types/modules/condition-manager.d.ts
+++ types/modules/condition-manager.d.ts
@@ -5,53 +5,79 @@ import TriggerType = Effects.TriggerType;
 import TriggersObject = Effects.TriggersObject;
 
 type PresetValue = {
-  value: string;
-  display: string;
+    value: string;
+    display: string;
 };
 
 type ConditionValueType = "text" | "number" | "preset" | "none";
 
 export type ConditionSettings<
-  ComparisonTypes extends string,
-  LeftSideValueType extends ConditionValueType,
-  RightSideValueType extends ConditionValueType,
+    ComparisonTypes extends string,
+    LeftSideValueType extends ConditionValueType,
+    RightSideValueType extends ConditionValueType
 > = {
-  type: string;
-  comparisonType: ComparisonTypes;
-  leftSideValue?: LeftSideValueType extends "number" ? number : string;
-  rightSideValue: RightSideValueType extends "number" ? number : string;
+    type: string;
+    comparisonType: ComparisonTypes;
+    leftSideValue: LeftSideValueType extends "number"
+        ? number
+        : LeftSideValueType extends "none"
+        ? undefined
+        : string;
+    rightSideValue: RightSideValueType extends "number" ? number : string;
 };
 
 export type ConditionType<
-  ComparisonTypes extends string = "",
-  LeftSideValueType extends ConditionValueType,
-  RightSideValueType extends ConditionValueType,
+    ComparisonTypes extends string,
+    LeftSideValueType extends ConditionValueType,
+    RightSideValueType extends ConditionValueType
 > = {
-  id: string;
-  name: string;
-  description: string;
-  comparisonTypes: ComparisonTypes[];
-  rightSideValueType: RightSideValueType;
-  leftSideValueType?: LeftSideValueType;
-  triggers?: TriggerType[] | TriggersObject;
-  leftSideTextPlaceholder?: string;
-  rightSideTextPlaceholder?: string;
-  getRightSidePresetValues?: (...args: unknown) => PresetValue[];
-  getLeftSidePresetValues?: (...args: unknown) => PresetValue[];
-  getRightSideValueDisplay?(
-    condition: ConditionSettings<ComparisonTypes, LeftSideValueType, RightSideValueType>,
-    ...args: unknown
-  ): string;
-  getLeftSideValueDisplay?(condition: ConditionSettings<ComparisonTypes, LeftSideValueType, RightSideValueType>, ...args: unknown): string;
-  valueIsStillValid?(condition: ConditionSettings<ComparisonTypes, LeftSideValueType, RightSideValueType>, ...args: unknown): boolean;
-  predicate(
-    conditionSettings: ConditionSettings<ComparisonTypes, LeftSideValueType, RightSideValueType>,
-    trigger: Trigger
-  ): boolean | Promise<boolean>;
+    id: string;
+    name: string;
+    description: string;
+    comparisonTypes: ComparisonTypes[];
+    rightSideValueType: RightSideValueType;
+    leftSideValueType?: LeftSideValueType;
+    triggers?: TriggerType[] | TriggersObject;
+    leftSideTextPlaceholder?: string;
+    rightSideTextPlaceholder?: string;
+    getRightSidePresetValues?: (...args: unknown[]) => PresetValue[];
+    getLeftSidePresetValues?: (...args: unknown[]) => PresetValue[];
+    getRightSideValueDisplay?(
+        condition: ConditionSettings<
+            ComparisonTypes,
+            LeftSideValueType,
+            RightSideValueType
+        >,
+        ...args: unknown[]
+    ): string;
+    getLeftSideValueDisplay?(
+        condition: ConditionSettings<
+            ComparisonTypes,
+            LeftSideValueType,
+            RightSideValueType
+        >,
+        ...args: unknown[]
+    ): string;
+    valueIsStillValid?(
+        condition: ConditionSettings<
+            ComparisonTypes,
+            LeftSideValueType,
+            RightSideValueType
+        >,
+        ...args: unknown[]
+    ): boolean;
+    predicate(
+        conditionSettings: ConditionSettings<
+            ComparisonTypes,
+            LeftSideValueType,
+            RightSideValueType
+        >,
+        trigger: Trigger
+    ): boolean | Promise<boolean>;
 };
 
 export type ConditionManager = EventEmitter & {
-  registerConditionType(conditionType: ConditionType): void;
-  getConditionTypeById(conditionTypeId: string): ConditionType;
-  getAllConditionTypes(): ConditionType[];
+    registerConditionType(conditionType: ConditionType<any, any, any>): void;
+    getConditionTypeById(conditionTypeId: string): ConditionType<any, any, any>;
+    getAllConditionTypes(): ConditionType<any, any, any>[];
 };
