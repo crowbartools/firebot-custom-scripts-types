64a4f9910de21d6c7948154974ae4eaafc56594d
Lee <phroggster@users.noreply.github.com>
2024-08-03 14:08:56 -0500

    feat: Add getEffectById to EffectManager (#29)
    * Add getEffectById to EffectManager
    
    Fixes #25.
    
    * The template approach is better in practice

--- types/modules/effect-manager.d.ts
+++ types/modules/effect-manager.d.ts
@@ -2,5 +2,6 @@ import { Effects } from "../effects";
 import EffectType = Effects.EffectType;
 
 export type EffectManager = {
+    getEffectById: <EffectModel>(effectId: string) => EffectType<EffectModel> | undefined;
     registerEffect: <EffectModel>(effectType: EffectType<EffectModel>) => void;
 };
