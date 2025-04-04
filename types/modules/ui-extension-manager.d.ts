3f0373a93beb1bb13ad70d81143fef9cc95d46cc
phroggster <phroggster@users.noreply.github.com>
2025-04-04 09:05:30 -0500

    feat: UI Extension Manager (Firebot/#2905)

---
+++ types/modules/ui-extension-manager.d.ts
@@ -0,0 +1,68 @@
+export interface BasePage {
+    id: string;
+    name: string;
+    icon: string;
+}
+
+export interface AngularJsPage extends BasePage {
+    type: "angularjs";
+    template: string;
+    controller: Function;
+}
+
+export interface AngularJsFactory {
+    name: string;
+    function: Function;
+}
+
+export interface AngularJsComponent {
+    name: string;
+    transclude?: boolean | string | {[slot: string]: string};
+    bindings: Record<string, string>;
+    template: string;
+    controller: Function;
+}
+
+export interface AngularJsDirective {
+    name: string;
+    function: (...args: unknown[]) => {
+        compile?: Function;
+        controller?: Function;
+        link?: Function;
+        multiElement?: boolean;
+        name?: string;
+        priority?: number;
+        require?: string | string[] | {[controller: string]: string};
+        restrict?: string;
+        scope?: boolean | object;
+        template?: string;
+        terminal?: boolean;
+        transclude?: boolean | string | {[slot: string]: string};
+    };
+}
+
+export interface AngularJsFilter {
+    name: string;
+    function: { (...args: unknown[]): Function; };
+}
+
+export interface UIExtension {
+    /** A unique identifier for this UI extension. */
+    id: string;
+    /** Add new sidebar entries under an "Extensions" category */
+    pages?: AngularJsPage[];
+    /** Add your own AngularJS services, components, directives, filters */
+    providers?: {
+        components?: AngularJsComponent[];
+        directives?: AngularJsDirective[];
+        factories?: AngularJsFactory[];
+        filters?: AngularJsFilter[];
+    };
+}
+
+export type UIExtensionManager = {
+    /** Register a third-party custom UI extension in Firebot.
+     * @param extension The custom UIExtension to register.
+     */
+    registerUIExtension(extension: UIExtension): void;
+};
